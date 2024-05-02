694c6e2bf0f7b30e2f2b19423dc98af353f0c9b5
Admin <nisha.soni@helixtechinc.com>
2024-05-02 12:22:33 -0400

    first commit

---
+++ src/components/Description.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Description(){
+    return(
+        <div>
+            <p>
+                Description of Pokemon
+            </p>
+        </div>
+    );
+}
+
+export default Description;
---
+++ src/components/PokemonThumbnail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function PokemonThumbnail(){
+    return(
+        <div>
+            <img src="path_to_pokemon_image" alt="Pokemon"/>
+            <p>POKEMON NAME</p>
+        </div>
+    );
+}
+export default PokemonThumbnail;
